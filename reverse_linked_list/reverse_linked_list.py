1558ba7657b88a2dc8e5e43f9cae80dba9290ee5
Arin Flores <chicanacodes@gmail.com>
2025-07-29 19:24:33 -0700

    linkedlists and num islands

---
+++ reverse_linked_list/reverse_linked_list.py
@@ -0,0 +1,21 @@
+def reverse_linked_list(head):
+    '''
+    Given a linked list, reverse the list and return the reversed list. 
+
+    Edge Cases:
+    - Can the list be empty?
+    - What values can the list contain?
+    - What is the maximum length of the list?
+    - What is the minimum length of the list?
+    - What is the expected input format?
+    - What is the expected output format?
+
+
+    Approach: 
+    - Iterate through the list and reverse the pointers of the nodes.
+    - Return the new head of the reversed list.
+    '''
+    pass
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
